a2e680ace4e982c8818b70327a9be8e5b6fbb359
ProGamerGov <ProGamerGov@users.noreply.github.com>
2016-11-05 14:08:42 -0600

    Update pixelator.sh

--- pixelator.sh
+++ pixelator.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
 
 main(){
-	# 1. defines
+	# 1. input image
 	input=$1
 	input_file=`basename $input`
 	clean_name="${input_file%.*}"
 
-convert -scale 10% -scale 1000% $input_file "$clean_name"_"pixelated".png
+
+	# 2. First scale value
+	scale=$2
+
+
+convert -scale "$scale""%" -scale 1000% $input_file "$clean_name"_"pixelated_""scale_""$scale".png
 
 }
 
 retry=0
-main $1
+main $1 $2
