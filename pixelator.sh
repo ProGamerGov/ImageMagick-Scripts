f08e8f679ae12c9624c148b11527a2b1c04dd833
ProGamerGov <ProGamerGov@users.noreply.github.com>
2016-11-05 19:19:57 -0600

    Update pixelator.sh

--- pixelator.sh
+++ pixelator.sh
@@ -9,9 +9,12 @@ main(){
 
 	# 2. First scale value
 	scale=$2
+	
+	width=`convert $intput_file -format "%w" info:`
+	height=`convert $input_file -format "%h" info:`
 
+convert -scale "$scale""%" -scale 1000% -resize "$width"x"$height" $input_file "$clean_name"_"pixelated_""scale_""$scale".png
 
-convert -scale "$scale""%" -scale 1000% $input_file "$clean_name"_"pixelated_""scale_""$scale".png
 
 }
 
